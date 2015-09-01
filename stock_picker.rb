34bedb4b13e42c608c744a2ca268dd63dd092c23
Carlos Quiroga <cquiro@gmail.com>
2015-08-31 21:33:37 -0500

    Adds comments and cleans code in stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,44 +1,34 @@
-#stock_picker.rb
+#Array of stock prices. Each element represents a price for that day (index represents day number).
+stock_prices = [17, 3, 6, 9, 15, 8, 6, 30, 10]
 
-#stock_prices = [17,3,6,9,15,8,6,30,10]
-
-    
-
-    
-
-i = 0
-profit = stock_prices[1] - stock_prices[0]
+#Initializes the array that keeps track of the days when profit is higher
+#with the first two days in the aray.
 days_prof = [0, 1]
 
-until buy == stock_prices.length - 1
-    sell = buy + 2
-    until sell ==  stock_prices.length
-		if profit < stock_prices[sell] - stock_prices[buy]
-            profit = stock_prices[sell] - stock_prices[buy] 
-            days_prof = [buy, sell]
-            sell += 1
-        else
-        	sell += 1
-        end
+#Initilizes the buy and sell days with the days_prof array.
+buy_day = days_prof[0]
+sell_day = days_prof[1]
+
+#Initializes profit for the first two days. (buy_day = 0, sell_day = 1)
+profit = stock_prices[sell_day] - stock_prices[buy_day]
+
+#buy_day can never be the last day
+until buy_day == stock_prices.length - 1
+  #Sets new sell day to one day after new buy_day to start comparing from there.
+  sell_day = buy_day + 1
+  #sell_day should not be greater than the array length.
+  until sell_day == stock_prices.length
+    #calculates new profit between the current buy_day and all the possible sell days.
+    new_profit = stock_prices[sell_day] - stock_prices[buy_day]
+    if profit < new_profit
+      profit = new_profit #updates profit if new_profit is higher than profit.
+      days_prof = [buy_day, sell_day] #updates days_prof array with the new buy and sell days.
+      sell_day += 1
+    else
+    	sell_day += 1
     end
-    buy += 1
+  end
+  buy_day += 1
 end
 
-p days_prof
-
-
-#This is not a solution for this problem. This is 
-#the Ruby way to go throgh the square.
-=begin
-stocks = [17,3,6,9,15,8,6,30,10] 
-stocks.size.times do |buy|
-    stocks.size.times do |sell|
-        p "#{buy}, #{sell}"
-    end
-end
-=end
-
-
-
-
-
+p "You should buy stock on day #{days_prof[0]} and sell it on day #{days_prof[1]}."
